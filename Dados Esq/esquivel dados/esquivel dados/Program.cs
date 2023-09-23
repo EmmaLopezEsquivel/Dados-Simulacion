10c3a75c5448bad6d1b199fad196b31636dc402e
EmmaLopezEsquivel <123218003+EmmaLopezEsquivel@users.noreply.github.com>
2023-09-23 16:11:51 -0600

    Add files via upload

---
+++ Dados Esq/esquivel dados/esquivel dados/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System; 
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "Simulador de Lanzamiento de Dados"; // esta parte del codigo es el título de la consola.
+        Console.ForegroundColor = ConsoleColor.Yellow; // aqui agrego el color de texto en amarillo.
+        Console.WriteLine("**************************************************");
+        Console.WriteLine("Bienvenido al Simulador de Lanzamiento de Dados!"); // agregue instrucciones en un mensaje de bienvenida.
+        Console.WriteLine("Presiona Enter para lanzar los dados o Esc para salir.");
+        Console.WriteLine("**************************************************");
+
+        Random random = new Random(); //Procedi a crear instancia para generar números aleatorios.
+
+        while (true) // utilize While para el bucle infinito que permita múltiples lanzamientos de dados.
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(); // esta parte espera confirmacion de que tecla presiona el usuario.
+
+            if (keyInfo.Key == ConsoleKey.Escape) // Verificar si se presionó la tecla Esc.
+            {
+                Console.WriteLine("\nSaliendo del programa..."); // Si el usuario presiono Esc manda un mensaje de salida.
+                break; // terminar el programa.
+            }
+            else if (keyInfo.Key == ConsoleKey.Enter) // Verificar si se presionó la tecla Enter.
+            {
+                int dado1 = random.Next(1, 7); // Generar un número aleatorio entre 1 y 6 para el dado 1.
+                int dado2 = random.Next(1, 7); // Generar un número aleatorio entre 1 y 6 para el dado 2.
+
+                Console.WriteLine($"\nDado 1: {dado1}"); // Muestra el resultado del dado 1.
+                Console.WriteLine($"Dado 2: {dado2}"); // Muestra el resultado del dado 2.
+                Console.WriteLine($"Total: {dado1 + dado2}\n"); // Muestra la suma de los resultados.
+            }
+            else
+            {
+                Console.WriteLine("\nPresiona Enter para lanzar los dados o Esc para salir.");
+                // Muestra un mensaje de instrucción si se presionó una tecla diferente.
+            }
+        }
+    }
+}
---
+++ Dados Esq/esquivel dados/esquivel dados/README.md
@@ -0,0 +1,28 @@
+# Simulador de Lanzamiento de Dados en C#
+
+Este proyecto es un simulador de lanzamiento de dados en C#. 
+A continuación, Te detallo sus características y cómo funciona:
+
+## Descripción
+
+Este programa en C# simula el lanzamiento de dos dados virtuales y muestra los resultados en la consola.
+Los usuarios pueden lanzar los dados presionando la tecla Enter y salir del programa en cualquier momento presionando la tecla Esc.
+
+## Funcionalidades
+
+- **Lanzamiento de Dados**: El programa genera números aleatorios entre 1 y 6 para simular el lanzamiento de dos dados. Los resultados se muestran en la consola.
+
+- **Interfaz Amigable**: La consola muestra un mensaje de bienvenida y las instrucciones para lanzar los dados o salir del programa.
+
+- **Salida Controlada**: El programa permite a los usuarios salir en cualquier momento presionando la tecla Esc.
+
+
+## Autor
+
+- [Enmanuel Esquivel](https://github.com/tu-usuario)
+
+## Licencia
+
+Este proyecto está bajo la [Licencia MIT](LICENSE).
+
+Espero que este README te ayude a comprender el proyecto y cómo utilizarlo. Si tienes alguna pregunta o necesitas más información, no dudes en contactarme. ¡Disfruta del simulador de lanzamiento de dados en C#!
